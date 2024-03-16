2c8043c33ae74f203ec29492989cd0aabcef064d
Shin Young Lee <161694701+2shin0@users.noreply.github.com>
2024-03-16 18:28:38 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,19 @@
+select emp_no, emp_name
+,   case 
+        when avg(score) >= 96 then 'S'
+        when avg(score) >= 90 then 'A'
+        when avg(score) >= 80 then 'B'
+        else 'C'
+    end as grade
+,   sal *
+    case 
+        when avg(score) >= 96 then 0.2
+        when avg(score) >= 90 then 0.15
+        when avg(score) >= 80 then 0.1
+        else 0
+    end as bonus
+from hr_employees 
+join hr_grade using(emp_no)
+join hr_department using(dept_id)
+group by emp_no
+order by emp_no;
