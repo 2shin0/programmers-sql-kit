5641f999ecf172e7d752de6eda47b8dd85a236c0
Shin Young Lee <02.shin.00@gmail.com>
2024-07-21 19:27:38 +0900

    [level 2] Title: 올바른 괄호, Time: 7.41 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack = []
+    
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    
+    return not stack
