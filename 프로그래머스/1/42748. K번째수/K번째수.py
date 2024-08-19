b2a95adc282188dce5066c05b3b7306f7f5b094f
Shin Young Lee <02.shin.00@gmail.com>
2024-08-19 10:22:46 +0900

    Delete 프로그래머스/1/42748. K번째수 directory

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++
@@ -1,9 +0,0 @@
-def solution(array, commands):
-    answer = []
-    for command in commands:
-        i, j, k = command
-        # 배열의 i번째부터 j번째까지 자르고 정렬
-        sliced_array = sorted(array[i-1:j])
-        # 정렬된 배열에서 k번째 숫자를 answer에 추가
-        answer.append(sliced_array[k-1])
-    return answer
